cd7d06a7fe25f588266a8e442a723119b56c783d
Kerem Çalışkan <caliskan.kerem.57@gmail.com>
2025-03-22 20:54:18 +0300

    SoundSystem

---
+++ Simple/SoundSystem/README.md
@@ -0,0 +1,9 @@
+# Simple-SaveSystem
+
+Simple Sound System for Unity
+
+# How To Use
+
+- Add SoundSystem.cs to your scene
+- Adjust your sounds list in inspector
+- Call Simple.SoundSystem.Instance.PlaySound(Simple.SoundType.Example)
---
+++ Simple/SoundSystem/SoundSystem.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+namespace Simple
+{
+    public enum SoundType { Example }
+
+    [DefaultExecutionOrder(-2)]
+    public class SoundSystem : MonoBehaviour
+    {
+        public static SoundSystem Instance { get; private set; }
+
+        public bool On = true;
+        public float MultiplayThrehold = 0.1f;
+
+        [SerializeField] private Sound[] _sounds;
+
+        private AudioSource _audioSource;
+
+        private void OnValidate()
+        {
+            if (_sounds == null)
+                return;
+            if (_sounds.Length <= 0)
+                return;
+
+            for (int i = 0; i < _sounds.Length; i++)
+            {
+                _sounds[i].Name = _sounds[i].Type.ToString();
+            }
+        }
+
+        [System.Serializable]
+        public class Sound
+        {
+            [HideInInspector] public string Name;
+            [field: SerializeField] public SoundType Type { get; private set; }
+            [Range(0, 3)]
+            [SerializeField] public float Volume = 1;
+            [SerializeField] private AudioClip[] _clips;
+
+            [HideInInspector] public float LastPlay;
+            private int _clipIndex;
+
+            public AudioClip GetClip()
+            {
+                _clipIndex++;
+                if (_clipIndex >= _clips.Length)
+                    _clipIndex = 0;
+
+                return _clips[_clipIndex];
+            }
+        }
+
+        private void Awake()
+        {
+            Instance = this;
+
+            _audioSource = gameObject.AddComponent<AudioSource>();
+            _audioSource.playOnAwake = false;
+            _audioSource.loop = false;
+        }
+
+        public void PlaySound(SoundType sound)
+        {
+            if (!On)
+                return;
+
+            Sound s = GetSound(sound);
+            if (s == null)
+            {
+                Debug.LogWarning(sound + " is not attached to SoundManager.");
+                return;
+            }
+            float currentTime = Time.time;
+            if ((currentTime - s.LastPlay) < MultiplayThrehold)
+                return;
+
+            s.LastPlay = currentTime;
+            _audioSource.PlayOneShot(s.GetClip(), s.Volume);
+        }
+
+        public Sound GetSound(SoundType type)
+        {
+            for (int i = 0; i < _sounds.Length; i++)
+            {
+                if (_sounds[i].Type == type)
+                    return _sounds[i];
+            }
+            return null;
+        }
+    }
+}
